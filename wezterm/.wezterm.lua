ce816adfed0bc5104200829297316c82fa328713
quocnguyen <quocnguyenx43@gmail.com>
2025-05-01 11:44:36 +0700

    updated

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,12 +7,20 @@ local mux = wezterm.mux
 -- Theme & font
 config.color_scheme = "Tokyo Night"
 config.window_background_opacity = 0.98
--- config.font = wezterm.font("JetBrains Mono NL")
+-- https://github.com/podkovyrin/JetBrainsMono/tree/feature/no-ligatures-1-0-3/no-ligatures
+config.font = wezterm.font_with_fallback(
+	{
+		"JetBrains Mono NL",
+		'JetBrains Mono',
+		'FiraCode Nerd Font',
+		'Menlo',
+	}
+)
 config.font_size = 11
 
 -- Window & tab
 config.enable_tab_bar = false
--- config.window_decorations = "NONE"
+-- config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 5,
 	right = 0,
@@ -26,38 +34,7 @@ wezterm.on("gui-startup", function()
 end)
 
 -- -- Others
--- config.adjust_window_size_when_changing_font_size = true
--- config.hide_mouse_cursor_when_typing = false
+config.adjust_window_size_when_changing_font_size = true
+config.hide_mouse_cursor_when_typing = false
 
--- -- Key bindings
--- config.keys = {
--- 	{
--- 		key = "c",
--- 		mods = "CTRL",
--- 		action = wezterm.action_callback(function(window, pane)
--- 			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
--- 			if has_selection then
--- 				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
--- 				window:perform_action(act.ClearSelection, pane)
--- 			else
--- 				window:perform_action(act.SendKey({ key = "c", mods = "CTRL" }), pane)
--- 			end
--- 		end),
--- 	},
--- }
-
--- -- Startup programs: Tmux
--- config.default_prog = {
--- 	"/usr/bin/zsh",
--- 	"-c",
--- 	[[
---     LAST_SESSION=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | tail -n 1)
---     if [ -n "$LAST_SESSION" ]; then
---       exec tmux attach-session -t "$LAST_SESSION"
---     else
---       exec tmux new-session -s new
---     fi
---   ]],
--- }
-
--- return config
+return config
