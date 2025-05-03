08807e7296295acd466a5dc8bda9a118116afdf6
quocnguyen <quocnguyenx43@gmail.com>
2025-05-03 18:23:52 +0700

    updated

--- wezterm/.wezterm_startup.sh
+++ wezterm/.wezterm_startup.sh
@@ -4,16 +4,9 @@ if command -v tmux >/dev/null 2>&1; then
     if [ -z "$TMUX" ]; then
         LAST_SESSION=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | tail -n 1)
         if [ -n "$LAST_SESSION" ]; then
-            exec tmux attach-session -t "$LAST_SESSION"
-            echo "attached to the last tmux session $LAST_SESSION."
+            tmux attach-session -t "$LAST_SESSION"
         else
-            exec tmux new-session -s new
-            echo "new tmux session created."
+            tmux new-session -s new
         fi
     fi
-else
-    echo "tmux is not installed. Please install tmux first."
-    sleep 100
 fi
-
-exec zsh
